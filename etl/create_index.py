8c6a259a74667eb21bcd68238908557a616939a8
TkachevAlex <itbr.taxes@gmail.com>
2026-03-01 18:31:36 +0300

    -Убрал опечатку

--- etl/create_index.py
+++ etl/create_index.py
@@ -6,7 +6,7 @@ from .config import settings
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-ES_URL = f"http://{settings.elastic_host}:{settings.elastic_port}/movies"dв
+ES_URL = f"http://{settings.elastic_host}:{settings.elastic_port}/movies"
 
 
 def create_index():
